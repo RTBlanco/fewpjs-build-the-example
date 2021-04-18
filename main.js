1d2562d05067c1611fb05d3465b5b25fc7280f1d
RTBlanco <ronnytoribio1@hotmail.com>
2021-04-18 00:16:20 -0400

    commit tree da4df1abe5b6901963a10b932d7ba3d50d68fe5e

--- main.js
+++ main.js
@@ -1,9 +1,32 @@
 // Defining text characters for the empty and full hearts for you to use later.
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
+const hearts = document.getElementsByClassName("like-glyph")
+const errorModal = document.getElementById("modal")
 
 // Your JavaScript code goes here!
-
+for (const btn of hearts){
+  btn.addEventListener('click', (e)=>{
+    e.preventDefault();
+    mimicServerCall()
+    .then(()=>{
+      if (!btn.classList.contains('activated-heart')){
+        btn.innerText = FULL_HEART;
+        btn.classList.add("activated-heart")
+      } else {
+        btn.innerText = EMPTY_HEART
+        btn.classList.remove('activated-heart')
+      }
+    })
+    .catch((error)=>{
+      errorModal.classList.remove("hidden")
+      errorModal.lastElementChild.innerText = error
+      setTimeout(()=>{
+        errorModal.classList.add("hidden")
+      },3000)
+    })
+  })
+}
 
 
 
